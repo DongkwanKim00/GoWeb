910f261d8260e0c3153d7a80cb0066a6324e5289
DongkwanKim00 <112566149+DongkwanKim00@users.noreply.github.com>
2023-05-28 20:07:50 +0900

    Update News.js

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import newsData from './newsData';
-import Hyperlink from 'react-native-hyperlink'
-import { Text, StyleSheet } from 'react-native'
+import Hyperlink from 'react-native-hyperlink';
 import openURL from './OpenUrl';
-import { useState } from "react";
-/**asdfsadfasdfds */
-const News = () => {
+import { Text } from 'react-native';
 
+const News = () => {
   const [showButton, setShowButton] = useState(false);
 
   const handleScroll = () => {
@@ -16,66 +14,65 @@ const News = () => {
       setShowButton(false);
     }
   };
-/*12313123131231231*/ 
+
   const handleClick = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  window.addEventListener("scroll", handleScroll);
-
-
+  window.addEventListener('scroll', handleScroll);
 
   return (
     <>
-      <div className="banner" style={{marginTop: "36px"}}>
+      <div className="banner" style={{ marginTop: '36px' }}>
         <h2 className="text-center">Drone News</h2>
-        
       </div>
 
-      <div className="news" style={{
-        background: `url('/cloudBackground.gif') no-repeat center center fixed`,
-        minHeight: "100vh",
-        padding: "50px",
-        marginTop: "80px"
-      }}>
+      <div
+        className="news"
+        style={{
+          backgroundImage: `url('/cloudBackground.gif')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+          minHeight: '100vh',
+          padding: '50px',
+          marginTop: '80px',
+        }}
+      >
         <ul>
           {newsData.map((newsItem) => (
             <tr key={newsItem.id}>
               <img src={newsItem.thumbnailUrl} alt={newsItem.title} style={{ width: '300px' }} />
               <p></p>
               <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <Hyperlink linkStyle={styles.hyperlinkStyle} onPress={(url) => openURL(url)}>
-                <Text style={styles.contentStyle} onPress={(url) => openURL(newsItem.link)}>{newsItem.title}</Text>
-              </Hyperlink>
+                <Hyperlink linkStyle={styles.hyperlinkStyle} onPress={(url) => openURL(url)}>
+                  <Text style={styles.contentStyle} onPress={(url) => openURL(newsItem.link)}>
+                    {newsItem.title}
+                  </Text>
+                </Hyperlink>
               </div>
               <hr></hr>
             </tr>
           ))}
         </ul>
 
-      <button
-        className={`top-button ${showButton ? "show" : ""}`}
-        onClick={handleClick}
-      >
-        Back to top
-      </button>
+        <button className={`top-button ${showButton ? 'show' : ''}`} onClick={handleClick}>
+          Back to top
+        </button>
       </div>
-  
     </>
   );
-}
+};
 
-const styles = StyleSheet.create({
+const styles = {
   hyperlinkStyle: {
     fontSize: 16,
-    color: '#505050'
+    color: '#505050',
   },
   contentStyle: {
     fontSize: 18,
-    color: '#111111'
-  }
-})
-
+    color: '#111111',
+  },
+};
 
 export default News;
-
